bc3896b151f05d2b37b78621f4f756fcaeca2c71
Rashod Chamikara <chamikara2008@gmail.com>
2022-05-31 11:49:07 +0530

    Contact is partially done

--- wp-content/themes/AAA-WP/template-contact.php
+++ wp-content/themes/AAA-WP/template-contact.php
@@ -9,186 +9,75 @@
  */
 get_header();
 ?>
-<div id="carouselExampleCaptions2" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-inner">
-        <?PHP if (have_rows('main_slider')) {
-            $x = 1;
-            while (have_rows('main_slider')) : the_row(); ?>
-                <div class="carousel-item <?PHP if ($x == 1) {
-                                                echo 'active';
-                                            } ?> ">
-                    <img src="<?PHP echo get_sub_field('slider_image'); ?>" class="d-block w-100" alt="..." />
-                    <div class="carousel-caption-custom">
-                        <h5><?PHP echo get_sub_field('slider_title'); ?></h5>
-                        <p>
-                            <?PHP echo get_sub_field('slider_text'); ?>
-                        </p>
-                        <?PHP if (get_sub_field('slider_cta_text') != "" && get_sub_field('slider_cta_link') != "") { ?>
-                            <a class="call-to-action" href="<?PHP echo get_sub_field('slider_cta_link'); ?>"><?PHP echo get_sub_field('slider_cta_text'); ?></a>
-                        <?PHP } ?>
-                    </div>
-                </div>
-        <?PHP
-                $x++;
-            endwhile;
-        } ?>
-    </div>
-    <button class="left-arrow-custom" type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide="prev">
-        <i class="fa fa-long-arrow-left" aria-hidden="true"></i>
-        <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="right-arrow-custom" type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide="next">
-        <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
-        <span class="visually-hidden">Next</span>
-    </button>
+<!-- Hero banner -->
+<div class="hero-image">
+    <img src="<?PHP echo get_field('hero_image');?>" alt="" />
+    <div class="hero-title">Contact Us</div>
 </div>
-<section class="home-about grey">
-    <div class="home-section__wrapper">
-        <img src="<?PHP echo get_field('about_section_image'); ?>" alt="" />
-        <div class="section-content">
-            <div class="section-title"><?PHP echo get_field('about_section_title'); ?></div>
-            <div class="section-des">
-                <?PHP echo get_field('about_section_text'); ?>
+<!-- End Hero Banner -->
+<section class="contact">
+    <div class="contact-wrapper">
+        <div class="contact-wrapper__details">
+            <div class="contact-page-title">Contact us</div>
+            <div class="contact-contact-details">
+                <div class="footer-contact__item">
+                    <span><i class="fa fa-map-marker" aria-hidden="true" style="font-size: 20px; color: white"></i><?PHP echo get_field('address');?></span>
+                </div>
+                <div class="footer-contact__item">
+                    <span><i class="fa fa-phone" aria-hidden="true" style="font-size: 20px; color: white"></i><?PHP echo get_field('contact_number');?></span>
+                </div>
+                <div class="footer-contact__item">
+                    <span><i class="fa fa-envelope" aria-hidden="true" style="font-size: 18px; color: white"></i><?PHP echo get_field('email_address');?></span>
+                </div>
             </div>
-        </div>
-    </div>
-</section>
-<section class="home-about">
-    <div class="home-section__wrapper">
-        <div class="section-content">
-            <div class="section-title"><?PHP echo get_field('section_2_title'); ?></div>
-            <div class="section-des">
-                <?PHP echo get_field('section_2_text'); ?>
-            </div>
-        </div>
-        <img src="<?PHP echo get_field('section_2_image'); ?>" alt="" />
-    </div>
-</section>
-<section class="home-about orange">
-    <div class="home-section__wrapper">
-        <div class="image-wrapper">
-            <div class="images-flex">
-                <div class="image1"><img src="<?PHP echo get_field('join_with_us_image_1'); ?>" alt="" /></div>
-                <div class="image2"><img src="<?PHP echo get_field('join_with_us_image_2'); ?>" alt="" /></div>
-            </div>
-        </div>
 
-        <div class="section-content">
-            <div class="section-title how-to-join"><?PHP echo get_field('join_with_us_title'); ?></div>
-            <div class="section-des">
-                <?PHP echo get_field('join_with_us_text'); ?>
-            </div>
-            <a href="<?PHP echo get_field('join_with_us_cta_link'); ?>" class="join-now"><?PHP echo get_field('join_with_us_cta_text'); ?></a>
-        </div>
-    </div>
-</section>
-<?PHP if(get_field('block_section_title')!=""){?>
-<section class="how-we-help">
-    <div class="section-main-title"><?PHP echo get_field('block_section_title'); ?></div>
-    <div class="how-we-help__section-items">
-    <?PHP if (have_rows('block_items')) {
-        while (have_rows('block_items')) : the_row(); ?>
-        <div class="how-we-help_item">
-            <div class="item-image-wrapper"><img src="<?PHP echo get_sub_field('block_image'); ?>" alt="" /></div>
-
-            <div class="how-we-help__content">
-                <?PHP echo get_sub_field('block_text'); ?>
-            </div>
-        </div>
-        <?PHP endwhile;
-        }
-        ?>
-    </div>
-</section>
-<?PHP }?>
-<section class="news">
-    <div class="news-wrapper">
-        <div class="news-wrapper__controls">
-            <div class="sub-title"><?PHP echo get_field('post_slider_title'); ?></div>
-            <div class="arrow-wrapper">
-                <div class="left-arrow" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev"><i class="fa fa-long-arrow-left" aria-hidden="true"></i></div>
-                <div class="right-arrow" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next"><i class="fa fa-long-arrow-right" aria-hidden="true"></i></div>
+            <div class="contact-social">
+            <?PHP if(get_field('facebook_link')!=""){?>
+                <a href="<?PHP echo get_field('facebook_link');?>" target="_blank">
+                    <div class="contact-page-social__item"><i class="fa fa-facebook fa-3x" style="font-size: 24px; color: #d74c4b"></i></div
+                ></a>
+            <?PHP }?>
+            <?PHP if(get_field('instagram_link_')!=""){?>
+                <a href="<?PHP echo get_field('instagram_link_');?>" target="_blank">
+                    <div class="contact-page-social__item"><i class="fa fa-instagram fa-3x" style="font-size: 24px; color: #d74c4b"></i></div
+                ></a>
+            <?PHP }?>
+            <?PHP if(get_field('whatsapp_link')!=""){?>
+                <a href="<?PHP echo get_field('whatsapp_link');?>" target="_blank">
+                    <div class="contact-page-social__item"><i class="fa fa-whatsapp fa-3x" style="font-size: 24px; color: #d74c4b"></i></div
+                ></a>
+            <?PHP }?>
+            <?PHP if(get_field('twitter_link',83)!=""){?>
+                <a href="<?PHP echo get_field('twitter_link');?>" target="_blank">
+                    <div class="contact-page-social__item"><i class="fa fa-twitter fa-3x" style="font-size: 24px; color: #d74c4b"></i></div
+                ></a>
+            <?PHP }?>
             </div>
         </div>
-        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel" data-interval="false" data-pause="hover">
-            <!-- <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-            </div> -->
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <div class="news-cards">
-                        <div class="news-cards__wrapper">
-                            <div class="news-card">
-                                <img src="<?php echo get_template_directory_uri();?>/home-about.png" alt="" />
-                                <div class="news-title">Lorem ipsum</div>
-                                <div class="news-content">
-                                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is
-                                    that it has a more-or-less normal distribution of letters, as opposed to using 'Content here,
-                                </div>
-                                <div class="read-more"><a href="">Read more</a></div>
-                            </div>
-                            <div class="news-card">
-                                <img src="<?php echo get_template_directory_uri();?>/home-about.png" alt="" />
-                                <div class="news-title">Lorem ipsum</div>
-                                <div class="news-content">
-                                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is
-                                    that it has a more-or-less normal distribution of letters, as opposed to using 'Content here,
-                                </div>
-                                <div class="read-more"><a href="">Read more</a></div>
-                            </div>
-                        </div>
+        <div class="contact-form-wrapper">
+            <div class="contact-form__title"><?PHP echo get_field('form_title');?></div>
+            <div class="contact-page__des">
+                <?PHP echo get_field('form_description');?>
+            </div>
+            <div class="contact-form">
+                <form action="">
+                    <div class="form-input__item">
+                        <input type="text" placeholder="Name" />
                     </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="news-cards">
-                        <div class="news-cards__wrapper">
-                            <div class="news-card">
-                                <img src="<?php echo get_template_directory_uri();?>/home-about.png" alt="" />
-                                <div class="news-title">Lorem ipsum</div>
-                                <div class="news-content">
-                                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is
-                                    that it has a more-or-less normal distribution of letters, as opposed to using 'Content here,
-                                </div>
-                                <div class="read-more"><a href="">Read more</a></div>
-                            </div>
-                            <div class="news-card">
-                                <img src="<?php get_template_directory_uri();?>/home-about.png" alt="" />
-                                <div class="news-title">Lorem ipsum</div>
-                                <div class="news-content">
-                                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is
-                                    that it has a more-or-less normal distribution of letters, as opposed to using 'Content here,
-                                </div>
-                                <div class="read-more"><a href="">Read more</a></div>
-                            </div>
-                        </div>
+                    <div class="form-input__item">
+                        <input type="text" placeholder="Contact number" />
                     </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="news-cards">
-                        <div class="news-cards__wrapper">
-                            <div class="news-card">
-                                <img src="<?php echo get_template_directory_uri();?>/home-about.png" alt="" />
-                                <div class="news-title">Lorem ipsum</div>
-                                <div class="news-content">
-                                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is
-                                    that it has a more-or-less normal distribution of letters, as opposed to using 'Content here,
-                                </div>
-                                <div class="read-more"><a href="">Read more</a></div>
-                            </div>
-                            <div class="news-card">
-                                <img src="<?php  echo get_template_directory_uri();?>/home-about.png" alt="" />
-                                <div class="news-title">Lorem ipsum</div>
-                                <div class="news-content">
-                                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is
-                                    that it has a more-or-less normal distribution of letters, as opposed to using 'Content here,
-                                </div>
-                                <div class="read-more"><a href="">Read more</a></div>
-                            </div>
-                        </div>
+                    <div class="form-input__item">
+                        <input type="email" placeholder="E-mail" />
                     </div>
-                </div>
+                    <div class="form-input__item">
+                        <input type="text" placeholder="Address" />
+                    </div>
+                    <div class="form-input__message">
+                        <textarea name="" id="" cols="30" rows="10">Message</textarea>
+                    </div>
+                    <div class="join-us__submit-btn">Submit</div>
+                </form>
             </div>
         </div>
     </div>
